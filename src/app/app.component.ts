4cb8d4129239ea477800d56c25e0cdb68b640eb5
Dan1ell1 <daniellisz.silva@gmail.com>
2024-10-29 15:23:36 -0300

    ajustanto com diretivas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,27 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [CommonModule, FormsModule, RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'diretivas';
+  nome: string = '';
+
+  alunos = [
+    { nome: 'João', idade: 20 },
+    { nome: 'Maria', idade: 22 },
+    { nome: 'Pedro', idade: 19},
+  ];
+  mostrarTexto = false;
+
+  tornarTextoVisivel(): void{
+    this.mostrarTexto = true;
+  }
+  cor = 'azul'
 }
